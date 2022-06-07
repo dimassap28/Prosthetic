3ec5cb9d22508888b50f8553cf97b9fda74db32b
bismaanassuka <bismaanassuka@gmail.com>
2022-06-08 03:06:21 +0700

    menus styling and tidy up appr

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prosthesis/theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 String name = 'Doel';
 
@@ -10,9 +11,41 @@ void main() {
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
+  Widget menus({
+    required String image,
+    required String menuTitle,
+  }) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(24),
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            border: Border.all(
+              width: 1,
+              color: graycolor,
+            ),
+          ),
+          child: SvgPicture.asset(
+            image,
+            color: bluecolor,
+          ),
+        ),
+        Text(
+          menuTitle,
+          style: blacktTextStyle.copyWith(fontSize: 12, fontWeight: normal),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
         // bottom: true,
@@ -22,97 +55,124 @@ class Dashboard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                color: Colors.amber,
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Hello, $name!",
-                        ),
-                        const Text(
-                          "Sunday, 28 Juni 2022",
-                        )
-                      ],
-                    ),
-                    const Spacer(),
-                    Image.asset("assets/icon/dashboard/profile.png")
-                  ],
-                ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Hello, $name!",
+                            style: blacktTextStyle.copyWith(
+                              fontSize: 24,
+                              fontWeight: bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Sunday, 28 Juni 2022",
+                        style: blacktTextStyle.copyWith(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  Image.asset(
+                    "assets/icon/dashboard/profile.png",
+                    height: 60,
+                    fit: BoxFit.cover,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
               ),
               Image.asset(
                 "assets/icon/dashboard/slider.png",
                 width: double.infinity,
                 fit: BoxFit.fitWidth,
               ),
-              const Text(
-                "Menu",
-                style: TextStyle(),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 64,
+                child: TextField(
+                  maxLines: 1,
+                  style: blacktTextStyle.copyWith(fontSize: 16),
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                      filled: true,
+                      prefixIcon: SvgPicture.asset(
+                        "assets/icon/dashboard/Search.svg",
+                        color: blackcolor,
+                        width: 5,
+                        height: 5,
+                        fit: BoxFit.scaleDown,
+                      ),
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(25),
+                        ),
+                      ),
+                      fillColor:
+                          Theme.of(context).inputDecorationTheme.fillColor,
+                      contentPadding: EdgeInsets.zero,
+                      hintText: 'Search anything here',
+                      hintStyle: blacktTextStyle.copyWith(
+                          color: const Color.fromRGBO(54, 60, 60, 0.5))),
+                ),
+              ),
+              Text(
+                "Menus",
+                style: blacktTextStyle.copyWith(
+                  fontSize: 24,
+                  fontWeight: bold,
+                ),
               ),
               Row(
                 children: [
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        width: 1,
-                        color: graycolor,
-                      ),
-                    ),
-                    child: Icon(Icons.abc_rounded, color: bluecolor),
+                  menus(
+                    image: "assets/icon/dashboard/Paper.svg",
+                    menuTitle: "Prosthesis Information",
                   ),
                   const Spacer(),
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        width: 1,
-                        color: graycolor,
-                      ),
-                    ),
-                    child: Icon(Icons.abc_rounded, color: bluecolor),
+                  menus(
+                    image: "assets/icon/dashboard/Call.svg",
+                    menuTitle: "Local Providers",
                   ),
                   const Spacer(),
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        width: 1,
-                        color: graycolor,
-                      ),
-                    ),
-                    child: Icon(Icons.abc_rounded, color: bluecolor),
+                  menus(
+                    image: "assets/icon/dashboard/Call.svg",
+                    menuTitle: "International Providers",
                   ),
                   const Spacer(),
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        width: 1,
-                        color: graycolor,
-                      ),
-                    ),
-                    child: Icon(Icons.abc_rounded, color: bluecolor),
-                  ),
+                  menus(
+                      image: "assets/icon/dashboard/Heart.svg",
+                      menuTitle: "Prosthesis Rehabilitation")
                 ],
               ),
               Row(
                 children: [
-                  const Text("Articles"),
+                  Text(
+                    "Articles",
+                    style: blacktTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: bold,
+                    ),
+                  ),
                   const Spacer(),
                   TextButton(
                     onPressed: () {},
-                    child: const Text("See more"),
+                    child: Text(
+                      "See more",
+                      style: bluetTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: bold,
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -122,47 +182,61 @@ class Dashboard extends StatelessWidget {
                 ),
                 child: Row(
                   children: [
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      // height: 180,
-                      // width: 180,
-                      decoration: BoxDecoration(color: bluecolor),
-                      child: Column(
-                        children: [
-                          Image.asset("assets/icon/dashboard/thumbnail.jpg"),
-                          const Text("data"),
-                          const Text("data"),
-                        ],
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Image.asset(
+                            "assets/icon/dashboard/thumbnail1.png",
+                            width: 200,
+                            height: 176,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                        Text(
+                          "Kaki Palsu Prostetik",
+                          style: blacktTextStyle.copyWith(
+                            fontSize: 20,
+                            fontWeight: bold,
+                          ),
+                        ),
+                        Text(
+                          "lorem ipsum dolor sit",
+                          style: blacktTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: normal,
+                          ),
+                        ),
+                      ],
                     ),
                     const Spacer(),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      // height: 180,
-                      // width: 180,
-                      decoration: BoxDecoration(color: bluecolor),
-                      child: Column(
-                        children: [
-                          Image.asset("assets/icon/dashboard/thumbnail.jpg"),
-                          const Text("data"),
-                          const Text("data"),
-                        ],
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Image.asset(
+                            "assets/icon/dashboard/thumbnail.png",
+                            width: 200,
+                            height: 176,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                        Text(
+                          "Manfaat Kaki Palsu",
+                          style: blacktTextStyle.copyWith(
+                            fontSize: 20,
+                            fontWeight: bold,
+                          ),
+                        ),
+                        Text(
+                          "lorem ipsum dolor sit",
+                          style: blacktTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: normal,
+                          ),
+                        ),
+                      ],
                     ),
-                    // Container(
-                    //   padding: const EdgeInsets.all(10),
-                    //   width: double.infinity,
-                    //   height: 300,
-                    //   decoration: BoxDecoration(color: bluecolor),
-                    //   child: const Text("grid1"),
-                    // ),
-                    // Container(
-                    //   padding: const EdgeInsets.all(10),
-                    //   width: double.infinity,
-                    //   height: 300,
-                    //   decoration: BoxDecoration(color: bluecolor),
-                    //   child: const Text("grid2"),
-                    // )
                   ],
                 ),
               ),
