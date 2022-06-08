1d330ea68c6b6de55f891b59a5e11b71e05c2fcd
dimassap <dimasadisyah@gmail.com>
2022-06-08 14:31:12 +0700

    add local + inter providers

---
+++ lib/interProviders.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const Prosthesis(
+      // home: localproviders(),
+      ));
+}
+
+class Prosthesis extends StatelessWidget {
+  const Prosthesis({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Nunito'),
+      home: const Scaffold(
+        body: localProviders(),
+      ),
+    );
+  }
+}
+
+List<Map<String, String>> doctor = [
+  {
+    'image': 'assets/images/p11.png',
+    'name': 'Dr. Suparjo',
+    'subtitle': 'subtitle',
+    'special': 'Top Speciality Extrimity',
+    'loc': 'Mojoroto (1.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p12.png',
+    'name': 'Dr. Ammar',
+    'subtitle': 'subtitle',
+    'special': 'Bottom Speciality Extrimity',
+    'loc': 'Gambiran (3.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p13.png',
+    'name': 'Dr. Nathalia',
+    'subtitle': 'subtitle',
+    'special': 'Top & Bottom Extrimity',
+    'loc': 'Kilisuci (4.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p14.png',
+    'name': 'Dr. Joya',
+    'subtitle': 'subtitle',
+    'special': 'Bottom Speciality Extrimity',
+    'loc': 'Bhayangkara (5.2 km)',
+    'icon': 'assets/images/right1.svg',
+  }
+];
+
+class localProviders extends StatefulWidget {
+  const localProviders({super.key});
+
+  @override
+  State<localProviders> createState() => _localProvidersState();
+}
+
+class _localProvidersState extends State<localProviders> {
+  final padding = const EdgeInsets.symmetric(horizontal: 30);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Container(
+          padding: padding,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 30,
+                height: 30,
+                child: SvgPicture.asset(
+                  'assets/images/left.svg',
+                ),
+              ),
+              const Spacer(),
+              const Text(
+                'Local Providers',
+                style: TextStyle(
+                  color: Color(0xff363C3C),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Spacer(),
+              SizedBox(
+                width: 30,
+                height: 30,
+                child: SvgPicture.asset(
+                  'assets/images/filter.svg',
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        Container(
+          padding: padding,
+          width: 330,
+          height: 180,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          padding: padding,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'Local service provider',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: doctor.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                height: 100,
+                                child: Image(
+                                  image: AssetImage(
+                                    '${doctor[index]['image']}',
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${doctor[index]['name']}',
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${doctor[index]['subtitle']}',
+                                  ),
+                                  Text(
+                                    '${doctor[index]['special']}',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${doctor[index]['loc']}',
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              SizedBox(
+                                width: 25,
+                                height: 25,
+                                child: SvgPicture.asset(
+                                  '${doctor[index]['icon']}',
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/localProviders.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const Prosthesis(
+      // home: localproviders(),
+      ));
+}
+
+class Prosthesis extends StatelessWidget {
+  const Prosthesis({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        theme: ThemeData(fontFamily: 'Nunito'),
+        home: const Scaffold(
+          body: LocalProviders(),
+        ),
+      );
+    });
+  }
+}
+
+List<Map<String, String>> doctor = [
+  {
+    'image': 'assets/images/p11.png',
+    'name': 'Dr. Suparjo',
+    'subtitle': 'subtitle',
+    'special': 'Top Speciality Extrimity',
+    'loc': 'Mojoroto (1.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p12.png',
+    'name': 'Dr. Ammar',
+    'subtitle': 'subtitle',
+    'special': 'Bottom Speciality Extrimity',
+    'loc': 'Gambiran (3.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p13.png',
+    'name': 'Dr. Nathalia',
+    'subtitle': 'subtitle',
+    'special': 'Top & Bottom Extrimity',
+    'loc': 'Kilisuci (4.2 km)',
+    'icon': 'assets/images/right1.svg',
+  },
+  {
+    'image': 'assets/images/p14.png',
+    'name': 'Dr. Joya',
+    'subtitle': 'subtitle',
+    'special': 'Bottom Speciality Extrimity',
+    'loc': 'Bhayangkara (5.2 km)',
+    'icon': 'assets/images/right1.svg',
+  }
+];
+
+class LocalProviders extends StatefulWidget {
+  const LocalProviders({super.key});
+
+  @override
+  State<LocalProviders> createState() => _LocalProvidersState();
+}
+
+class _LocalProvidersState extends State<LocalProviders> {
+  final padding = const EdgeInsets.symmetric(horizontal: 20);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 3.5.h,
+                height: 3.5.h,
+                child: SvgPicture.asset(
+                  'assets/images/left.svg',
+                ),
+              ),
+              const Spacer(),
+              Text(
+                'Local Providers',
+                style: TextStyle(
+                  color: const Color(0xff363C3C),
+                  fontSize: 15.sp,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Spacer(),
+              SizedBox(
+                width: 3.5.h,
+                height: 3.5.h,
+                child: SvgPicture.asset(
+                  'assets/images/filter.svg',
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: 41.h,
+          height: 20.h,
+          decoration: BoxDecoration(
+            color: Color(0xffd9d9d9),
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Local service provider',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 15.sp,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: doctor.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Container(
+                            width: 42.h,
+                            height: 12.h,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: const Color(0xffD9D9D9).withOpacity(0.2),
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 12.h,
+                                  height: 12.h,
+                                  child: Image(
+                                    image: AssetImage(
+                                      '${doctor[index]['image']}',
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Spacer(),
+                                    Text(
+                                      '${doctor[index]['name']}',
+                                      style: TextStyle(
+                                        fontSize: 18.sp,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    Text(
+                                      '${doctor[index]['subtitle']}',
+                                      style: TextStyle(fontSize: 9.sp),
+                                    ),
+                                    Text(
+                                      '${doctor[index]['special']}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 9.sp,
+                                      ),
+                                    ),
+                                    Text(
+                                      '${doctor[index]['loc']}',
+                                      style: TextStyle(fontSize: 9.sp),
+                                    ),
+                                    const Spacer(),
+                                  ],
+                                ),
+                                const Spacer(),
+                                Padding(
+                                  padding: EdgeInsets.only(right: 15.sp),
+                                  child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: const Color(0xffDDDDDD),
+                                      ),
+                                      borderRadius: BorderRadius.circular(
+                                        8,
+                                      ),
+                                    ),
+                                    child: Container(
+                                      padding: const EdgeInsets.all(8),
+                                      child: SvgPicture.asset(
+                                        '${doctor[index]['icon']}',
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
