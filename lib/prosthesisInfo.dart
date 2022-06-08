9c15a02f8c3d594fb3559f275415d6c1c88fce7b
dimassap <dimasadisyah@gmail.com>
2022-06-08 19:53:57 +0700

    fix image carousel + adding prosthesis info page

---
+++ lib/prosthesisInfo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProsthesisInfo());
+}
+
+class ProsthesisInfo extends StatefulWidget {
+  const ProsthesisInfo({Key? key}) : super(key: key);
+
+  @override
+  State<ProsthesisInfo> createState() => _ProsthesisInfoState();
+}
+
+class _ProsthesisInfoState extends State<ProsthesisInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
